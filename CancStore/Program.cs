d13444087b4549f22f0cb4382d1f77b45cf981f0
vitaly <kovynev_v@mail.ru>
2022-04-25 21:24:42 +0300

    Добавлено логирование с помощью NLog

--- CancStore/Program.cs
+++ CancStore/Program.cs
@@ -1,28 +1,44 @@
 using CancStore.Models;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("Initializing main");
 
-// Add services to the container.
+try {
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreDbContext>(optionsBuilder => 
-    optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("CancStoreDbConnection")));
+    // Add services to the container.
 
-var app = builder.Build();
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<StoreDbContext>(optionsBuilder => 
+        optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("CancStoreDbConnection")));
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+} catch(Exception ex) {
+    logger.Error(ex, "Error in initializing, close application");
+    throw;
+} finally {
+    LogManager.Shutdown();
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
